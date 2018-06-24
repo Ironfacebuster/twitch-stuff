d1bd2c20c5b5796e6f4f23333f418dba8cbcdb28
Ironfacebuster <38227932+Ironfacebuster@users.noreply.github.com>
2018-06-24 03:14:59 -0400

    The custom JS for the Simian Jar

---
+++ Custom Jars/SimianFlips Jar/SimianJar.js
@@ -0,0 +1,12 @@
+//If using StreamLabs, enable Custom HTML/CSS
+//Don't include lines with double slashes, they don't do anything
+
+jarEl.src = `https://github.com/Ironfacebuster/twitch-stuff/blob/master/Custom%20Jars/SimianFlips%20Jar/glass-pint%20(1).png?raw=true`;
+cupBase[1] += 35;
+boxBody = new p2.Body({ mass: 0, position: [cupBase[0], cupBase[1]] });
+boxShape = new p2.Box({ width: 220, height: 30 });
+boxShape.material = this.materials.wall;
+boxBody.addShape(boxShape);
+this.world.addBody(boxBody);
+this.world.addBody(this.createRectangleBody([cupBase[0] + 110, cupBase[1] + 266], 600, 30, 85));
+this.world.addBody(this.createRectangleBody([cupBase[0] - 110, cupBase[1] + 266], 600, 30, -85));
