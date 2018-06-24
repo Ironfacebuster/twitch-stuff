57f173955e37a3d0a2cd8ed9d33ef1cfb4531c7f
Ironfacebuster <38227932+Ironfacebuster@users.noreply.github.com>
2018-06-24 16:00:28 -0400

    Update MasonJar.js

--- Custom Jars/Mason Jar/MasonJar.js
+++ Custom Jars/Mason Jar/MasonJar.js
@@ -1,1 +1,17 @@
-//Congrats on seeing this WIP jar!
+//This image didn't turn out as well as the StreamLabs one, so I'd use it on a bright(er) background
+
+jarEl.src = `https://github.com/Ironfacebuster/twitch-stuff/blob/master/Custom%20Jars/Mason%20Jar/masonJar.png?raw=true`;
+cupBase[1] += 1;
+boxBody = new p2.Body({ mass: 0, position: [cupBase[0], cupBase[1]] });
+boxShape = new p2.Box({ width: 155, height: 15 });
+boxShape.material = this.materials.wall;
+boxBody.addShape(boxShape);
+this.world.addBody(boxBody);
+this.world.addBody(this.createRectangleBody([cupBase[0] + 85, cupBase[1] + 105], 210, 15, 90));
+this.world.addBody(this.createRectangleBody([cupBase[0] - 85, cupBase[1] + 105], 210, 15, -90));
+this.world.addBody(this.createRectangleBody([cupBase[0] + 73, cupBase[1] + 225], 60, 15, -68));
+this.world.addBody(this.createRectangleBody([cupBase[0] - 75, cupBase[1] + 225], 60, 15, 68));
+this.world.addBody(this.createRectangleBody([cupBase[0] + 65, cupBase[1] + 260], 28, 15, 90));
+this.world.addBody(this.createRectangleBody([cupBase[0] - 65, cupBase[1] + 260], 28, 15, 90));
+
+//Jar image from google
