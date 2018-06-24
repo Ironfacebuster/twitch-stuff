c6ab3ae97730bf3600f673540374df5e45ab8704
Ironfacebuster <38227932+Ironfacebuster@users.noreply.github.com>
2018-06-24 14:46:24 -0400

    Update SimianJar.js

--- Custom Jars/SimianFlips Jar/SimianJar.js
+++ Custom Jars/SimianFlips Jar/SimianJar.js
@@ -1,6 +1,9 @@
 //If using StreamLabs, enable Custom HTML/CSS
 //Don't include lines with double slashes, they don't do anything
 
+//This is just a giant jar, for people who get TONS of bits.
+//The bits still glitch out though, but the idea of having a larger jar is cool, right?
+
 jarEl.src = `https://github.com/Ironfacebuster/twitch-stuff/blob/master/Custom%20Jars/SimianFlips%20Jar/glass-pint%20(1).png?raw=true`;
 cupBase[1] += 35;
 boxBody = new p2.Body({ mass: 0, position: [cupBase[0], cupBase[1]] });
